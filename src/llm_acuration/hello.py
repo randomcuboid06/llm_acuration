f81c738ce7a918fadc48b5467bce29423526f012
randomcuboid06 <pranavsusarla2@gmail.com>
2024-02-11 14:13:06 +0530

    documented hello function

--- src/llm_acuration/hello.py
+++ src/llm_acuration/hello.py
@@ -1,2 +1,7 @@
 def hello():
+    """
+    This is just a placeholder function
+    
+    You can access this documentation by using __doc__ on the function or by using help()
+    """
     print("Hello, Acuration World!")
